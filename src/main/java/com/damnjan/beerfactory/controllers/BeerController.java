4d77d39e84b5bbd169ec94284aa5b53d3b2eb699
Damnjan <damnjana1998@gmail.com>
2021-08-10 10:32:40 +0200

    Added Beer Controller with CRUD methods

---
+++ src/main/java/com/damnjan/beerfactory/controllers/BeerController.java
@@ -0,0 +1,54 @@
+package com.damnjan.beerfactory.controllers;
+
+import com.damnjan.beerfactory.exceptions.BeerNotFoundException;
+import com.damnjan.beerfactory.model.BeerModel;
+import com.damnjan.beerfactory.services.BeerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/beers")
+public class BeerController {
+
+    private final BeerService beerService;
+
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<BeerModel>> getAllBeers() {
+        return ResponseEntity.ok(beerService.getAllBeers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerModel> getBeerById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(beerService.getOneBeer(id));
+        } catch (BeerNotFoundException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity saveOneBeer(@RequestBody BeerModel beerModel) {
+        beerService.saveBeer(beerModel);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteBeerById(@PathVariable Long id) {
+        try {
+            beerService.deleteBeerById(id);
+            return ResponseEntity.ok().build();
+        } catch (BeerNotFoundException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
